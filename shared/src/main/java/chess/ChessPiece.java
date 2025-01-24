4538f6a0f238c289de73ff3fe4cfdfdae05e6ab2
Parker Johns <pknjohns@byu.edu>
2025-01-24 09:25:38 -0700

    Implemented most methods for CHessBoard, ChessPosition, and ChessMove. Passed bishopMoveUntilEdge() test

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,9 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -10,7 +13,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,14 +37,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +55,79 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        ChessPiece currentPiece = board.getPiece(myPosition);
+
+        HashSet<ChessMove> moves = new HashSet<>();
+
+        // get start position info
+        int startRow = myPosition.getRow();
+        int startCol = myPosition.getColumn();
+
+        //find out how far we can move left, right, up down
+        int left = startCol - 1;
+        int right = 8 - startCol;
+        int down = startRow - 1;
+        int up = 8 - startRow;
+
+        for (PieceType piece : PieceType.values()) {
+            if (currentPiece.getPieceType() == piece && piece == PieceType.BISHOP) {
+
+                //create ChessMove objects we can make (independent of check, stalemate, or checkmate)
+                int i = 1;
+                while (i < 8) {
+
+                    int newRowUp = startRow + i;
+                    int newRowDown = startRow - i;
+                    int newColRight = startCol + i;
+                    int newColLeft = startCol - i;
+
+                    if (newRowUp <= 8 && newColRight <= 8) {
+                        ChessPosition newPosition = new ChessPosition((startRow + i), startCol + i);
+                        ChessMove newMove = new ChessMove(myPosition, newPosition, null);
+                        moves.add(newMove);
+                    }
+                    if (newRowUp <= 8 && newColLeft >= 1) {
+                        ChessPosition newPosition = new ChessPosition((startRow + i), startCol - i);
+                        ChessMove newMove = new ChessMove(myPosition, newPosition, null);
+                        moves.add(newMove);
+                    }
+                    if (newRowDown >= 1  && newColRight <= 8) {
+                        ChessPosition newPosition = new ChessPosition(startRow - i, startCol + i);
+                        ChessMove newMove = new ChessMove(myPosition, newPosition, null);
+                        moves.add(newMove);
+                    }
+                    if (newRowDown >= 1  && newColLeft >= 1) {
+                        ChessPosition newPosition = new ChessPosition(startRow - i, startCol - i);
+                        ChessMove newMove = new ChessMove(myPosition, newPosition, null);
+                        moves.add(newMove);
+                    }
+                    i++;
+                }
+            }
+        }
+
+        return moves;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "pieceColor=" + pieceColor +
+                ", type=" + type +
+                '}';
     }
 }
