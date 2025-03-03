74af5509ac95e5620422cd98d4b59dc1c6c14005
Parker Johns <pknjohns@byu.edu>
2025-03-03 13:13:33 -0700

    Fixed ClearService to use 3 DAOs instead of one DAO to track all 3 databases

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,19 +1,22 @@
 package service;
 
-import dataaccess.DataAccess;
-import dataaccess.DataAccessException;
+import dataaccess.*;
 
 public class ClearService {
 
-    private final DataAccess dataAccess;
+    private final AuthDAO authDB;
+    private final GameDAO gameDB;
+    private final UserDAO userDB;
 
-    public ClearService(DataAccess dataAccess) {
-        this.dataAccess = dataAccess;
+    public ClearService(AuthDAO authDB, GameDAO gameDB, UserDAO userDB) {
+        this.authDB = authDB;
+        this.gameDB = gameDB;
+        this.userDB = userDB;
     }
 
     public void clearDB() throws DataAccessException {
-        dataAccess.deleteAllUsers();
-        dataAccess.deleteAllGames();
-        dataAccess.deleteAllTokens();
+        userDB.deleteAllUsers();
+        gameDB.deleteAllGames();
+        authDB.deleteAllTokens();
     }
 }
--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,8 +1,7 @@
 package service;
 
 import chess.ChessGame;
-import dataaccess.DataAccessException;
-import dataaccess.MemoryDataAccess;
+import dataaccess.*;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
@@ -13,36 +12,38 @@ public class ClearServiceTest {
 
     @Test
     void doClearDB() throws DataAccessException {
-        MemoryDataAccess dataAccess = new MemoryDataAccess();
+        MemoryAuthDAO authDB = new MemoryAuthDAO();
+        MemoryGameDAO gameDB = new MemoryGameDAO();
+        MemoryUserDAO userDB = new MemoryUserDAO();
 
         var user1 = new UserData("pk", "1234", "pk@cs.com");
         var user2 = new UserData("kk", "5678", "kk@cs.com");
         var user3 = new UserData("pp", "1234", "cs.com");
 
-        dataAccess.addUser(user1);
-        dataAccess.addUser(user2);
-        dataAccess.addUser(user3);
+        userDB.addUser(user1);
+        userDB.addUser(user2);
+        userDB.addUser(user3);
 
         var game1 = new GameData(1234, "white", "black", "test", new ChessGame());
         var game2 = new GameData(5678, "white", "black", "test", new ChessGame());
         var game3 = new GameData(2345, "white", "black", "test", new ChessGame());
 
-        dataAccess.addGame(game1);
-        dataAccess.addGame(game2);
-        dataAccess.addGame(game3);
+        gameDB.addGame(game1);
+        gameDB.addGame(game2);
+        gameDB.addGame(game3);
 
         var token1 = new AuthData("1234", "PK");
         var token2 = new AuthData("2345", "PP");
         var token3 = new AuthData("3456", "KK");
 
-        dataAccess.addToken(token1);
-        dataAccess.addToken(token2);
-        dataAccess.addToken(token3);
+        authDB.addToken(token1);
+        authDB.addToken(token2);
+        authDB.addToken(token3);
 
-        ClearService service = new ClearService(dataAccess);
+        ClearService service = new ClearService(authDB, gameDB, userDB);
         service.clearDB();
-        assertEquals(0, dataAccess.listUsers().size());
-        assertEquals(0, dataAccess.listGames().size());
-        assertEquals(0, dataAccess.listTokens().size());
+        assertEquals(0, userDB.listUsers().size());
+        assertEquals(0, gameDB.listGames().size());
+        assertEquals(0, authDB.listTokens().size());
     }
 }
