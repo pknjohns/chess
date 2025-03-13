e8e315be6007683b1b9339918fa3d5070c2dc581
Parker Johns <pknjohns@byu.edu>
2025-03-12 23:46:13 -0600

    changed mySqlUserDAO to use util methods and pass tests

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -8,12 +8,26 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import static java.sql.Types.NULL;
+import static dataaccess.MySqlDAOutil.*;
 
 public class MySqlUserDAO implements UserDAO {
 
     public MySqlUserDAO() throws DataAccessException {
-        configureUserDatabase();
+
+        String[] createStatements = {
+                """
+            CREATE TABLE IF NOT EXISTS  users (
+              `username` VARCHAR(255) NOT NULL,
+              `password` TEXT NOT NULL,
+              `email` TEXT NOT NULL,
+              `userData` TEXT NOT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+        };
+
+        configureDatabase(createStatements);
     }
 
     public UserData addUser(UserData user) throws DataAccessException {
@@ -68,52 +82,5 @@ public class MySqlUserDAO implements UserDAO {
     private UserData readUser(ResultSet rs) throws SQLException {
         String json = rs.getString("userData");
         return new Gson().fromJson(json, UserData.class);
-    }
-
-    private void executeUpdate(String statement, Object... params) throws DataAccessException {
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var ps = conn.prepareStatement(statement)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    switch (param) {
-                        case String p -> ps.setString(i + 1, p);
-                        case Integer p -> ps.setInt(i + 1, p);
-                        case UserData p -> ps.setString(i + 1, p.toString());
-                        case null -> ps.setNull(i + 1, NULL);
-                        default -> {
-                        }
-                    }
-                }
-                ps.executeUpdate();
-            }
-        } catch (SQLException e) {
-            throw new DataAccessException("Unable to update database: " + statement + e.getMessage());
-        }
-    }
-
-    private final String[] createStatements = {
-            """
-            CREATE TABLE IF NOT EXISTS  users (
-              `username` VARCHAR(255) NOT NULL,
-              `password` TEXT NOT NULL,
-              `email` TEXT NOT NULL,
-              `userData` TEXT NOT NULL,
-              PRIMARY KEY (`username`),
-              INDEX(username)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """
-    };
-
-    private void configureUserDatabase() throws DataAccessException {
-        DatabaseManager.createDatabase();
-        try (var conn = DatabaseManager.getConnection()) {
-            for (var statement : createStatements) {
-                try (var preparedStatement = conn.prepareStatement(statement)) {
-                    preparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException e) {
-            throw new DataAccessException("Unable to configure database: " + e.getMessage());
-        }
     }
 }
