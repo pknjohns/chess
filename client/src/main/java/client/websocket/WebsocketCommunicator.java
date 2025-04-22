80fdfddd7ac1ef3d01cf67cd96d2c0c9bb4932b0
Parker Johns <pknjohns@byu.edu>
2025-04-22 12:21:18 -0600

    Made websocketcommunicator class and implemented onMessage method. Made ServerMessageObserver class.

--- client/src/main/java/client/websocket/WebsocketCommunicator.java
+++ client/src/main/java/client/websocket/WebsocketCommunicator.java
@@ -1,13 +1,41 @@
 package client.websocket;
 
-import javax.websocket.Endpoint;
-import javax.websocket.EndpointConfig;
-import javax.websocket.Session;
+import com.google.gson.Gson;
+import facade.ResponseException;
+import websocket.messages.ServerMessage;
+
+import javax.websocket.*;
+import java.net.URI;
 
 public class WebsocketCommunicator extends Endpoint {
 
+    Session session;
+    private final int port;
+    private ServerMessageObserver observer;
+
+    public WebsocketCommunicator(int port, ServerMessageObserver observer) throws ResponseException {
+        this.port = port;
+        this.observer = observer;
+        try {
+            String wsServerUrl = "ws://localhost:" + port + "/ws";
+            URI wsURI = new URI(wsServerUrl);
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, wsURI);
+
+            this.session.addMessageHandler((MessageHandler.Whole<String>) message -> {
+                ServerMessage msg = new Gson().fromJson(message, ServerMessage.class);
+                observer.notify(msg);
+            });
+        } catch (Exception ex) {
+            throw new ResponseException(ex.getMessage());
+        }
+    }
 
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
     }
+
+    // need connect, make move, leave, and resign functions (and maybe get board function) for websocket communicator
+    // need a "listener" that calls observer.notify if server sends any messages
 }
